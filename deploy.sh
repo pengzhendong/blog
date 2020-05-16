df76e117ccca9180ccfc4c48656c2a2cf3c8500c
pengzhendong <275331498@qq.com>
2020-05-16 09:52:41 +0800

    Commit before deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git add . &&
 git commit -m "Commit before deploy" &&
 git push &&
 
-# rm -rf .deploy_git &&
-# hexo clean &&
+rm -rf .deploy_git &&
+hexo clean &&
 export HEXO_ALGOLIA_INDEXING_KEY=4287c8f8a629343c8d2212e108417ceb &&
 hexo algolia && hexo g && gulp && hexo d
